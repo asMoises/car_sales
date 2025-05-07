20267bb16adb6719ef68afdeb50571f2d1efcdd8
Moisés Silva <moiss.silva@outlook.com>
2025-05-06 21:34:32 -0300

    Criando validações com funções clean_value e clean_factory_year

--- CarProject/a_cars/forms.py
+++ CarProject/a_cars/forms.py
@@ -1,7 +1,7 @@
 from django import forms  
 from a_cars.models import Car, Brand
 
-# forma antiga e mais demorada para se usar form.
+# forma antiga e mais demorada para se usar form. Não está em uso
 class CarForm(forms.Form):
     model = forms.CharField(max_length=200, label='Modelo')
     brand = forms.ModelChoiceField(Brand.objects.all(), label='Marca')   # Quando for chave estrangeira, usar ModelChoiceField.
@@ -36,5 +36,19 @@ class CarModelForm(forms.ModelForm):
 
         # Lembrando que no modo anterior, foi preciso indicar o campo brand como chave estrangeira manualmente.
         # Aqui, o Django já faz isso automaticamente.
-        # 
-  
+        
+
+    # Validações executadas pela função is_valid() que está no ModelForm.
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+
+        if value < 8000: # type: ignore
+            self.add_error('value', 'O valor do carro deve ser maior que R$ 8.000,00.')
+        return value
+    
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('factory_year')
+
+        if factory_year < 1910: # type: ignore
+            self.add_error('factory_year', 'Não é possível cadastrar carros fabricados antes de 1910.')
+        return factory_year
