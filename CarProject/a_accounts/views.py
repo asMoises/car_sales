107a347d46ccdb6acfb0a2bd345c1d2fc3e785b3
Moisés Silva <moiss.silva@outlook.com>
2025-05-08 09:23:56 -0300

    Criação da tela de login, funções de login e logout

--- CarProject/a_accounts/views.py
+++ CarProject/a_accounts/views.py
@@ -1,7 +1,36 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 def register_view(request):
     user_form = UserCreationForm()
 
+    if request.method == 'POST':
+        user_form = UserCreationForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+            return redirect('login')  # Redirect to the login page after successful registration
+        else:
+            return render(request, 'register.html', {'user_form': user_form, 'error': True})
+
     return render(request, 'register.html',{'user_form': user_form}) 
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('cars_list')    
+        else:
+            login_form = AuthenticationForm()
+    else:
+        login_form = AuthenticationForm()
+
+    return render(request, 'login.html',{'login_form': login_form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
